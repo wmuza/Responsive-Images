5c71d7d6f7fc1a88889a2cfd8959dfa8cbe8af1c
wmuza <wmuza@meticulosity.co.za>
2018-07-21 00:49:37 +0200

    Adding Webp plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,123 @@
-/*
- After you have changed any settings for the responsive_images task,
- run this with one of these options:
-  "grunt" alone creates a new, completed images directory
-  "grunt clean" removes the images directory
-  "grunt responsive_images" re-processes images without removing the old ones
-*/
-
 module.exports = function(grunt) {
 
   grunt.initConfig({
     responsive_images: {
-      dev: {
+    //Resolution-Switching Cases here
+
+      resolution_switch_case: {
         options: {
           engine: 'im',
           sizes: [{
-            width: 800,
-            suffix: '_large_1x',
-            quality: 50
-          },		  
-		  {
+            /* Change these */
             width: 1600,
             suffix: '_large_2x',
             quality: 30
+          },
+          { width: 800,
+            suffix: '_large_1x',
+            quality: 30
+          },
+          { width: 1280,
+            suffix: '_medium_2x',
+            quality: 30
+          },
+          { width: 640,
+            suffix: '_medium_1x',
+            quality: 30
+          },
+          { width: 640,
+            suffix: '_small_2x',
+            quality: 30
+          },
+          { width: 320,
+            suffix: '_small_1x',
+            quality: 30
           }]
         },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
         files: [{
           expand: true,
           src: ['*.{gif,jpg,png}'],
           cwd: 'images_src/',
           dest: 'images/'
         }]
+      },
+
+
+
+
+
+    // Handling Art-Direction Cases here :)
+    large_art_direction: {
+        options: {
+            sizes: [{ width: 1600, suffix: '_large_2x', quality: 30}, {width: 800, suffix: 'large_1x', quality: 30}]
+        },
+        files: [{
+            expand: true,
+            src: ['**/large_*.{jpg,gif,png}'], //Selects all files that begins with 'large_'
+            cwd: 'images_src/art_direction',
+            dest: 'images/'
+        }]
+    },
+    medium_art_direction: {
+        options: {
+            sizes: [{ width: 1280, suffix: '_medium_2x', quality: 30}, {width: 640, suffix: 'medium_1x', quality: 30}]
+        },
+        files: [{
+            expand: true,
+            src: ['**/medium_*.{jpg,gif,png}'], //Selects all files that begins with 'medium_'
+            cwd: 'images_src/art_direction',
+            dest: 'images/'
+        }]
+    },
+    small_art_direction: {
+        options: {
+            sizes: [{ width: 640, suffix: '_small_2x', quality: 30}, {width: 320, suffix: 'small_1x', quality: 30}]
+        },
+        files: [{
+            expand: true,
+            src: ['**/small_*.{jpg,gif,png}'], //Selects all files that begins with 'small_'
+            cwd: 'images_src/art_direction',
+            dest: 'images/'
+        }]
+    },
+},
+
+ // WebP configuration - this convert all images in images/ folder to webp
+ // Make sure 'grunt-responsive-image' is run before 'grunt-webp' otherwise images in images/ foler will not be converted (as source for webp plugin is images/ folder)
+    webp: {
+      files: {
+        expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images/',
+          dest: 'images/'
+      },
+      options: {
+        binpath: require('webp-bin').path,
+        preset: 'photo',
+        verbose: true,
+        quality: 80,
+        alphaQuality: 80,
+        compressionMethod: 6,
+        segments: 4,
+        psnr: 42,
+        sns: 50,
+        filterStrength: 40,
+        filterSharpness: 3,
+        simpleFilter: true,
+        partitionLimit: 50,
+        analysisPass: 6,
+        multiThreading: true,
+        lowMemory: false,
+        alphaMethod: 0,
+        alphaFilter: 'best',
+        alphaCleanup: true,
+        noAlpha: false,
+        lossless: false
       }
     },
 
@@ -49,24 +137,13 @@ module.exports = function(grunt) {
       },
     },
 
-    /* Copy the "fixed" images that don't go through processing into the images/directory */
-    copy: {
-      dev: {
-        files: [{
-          expand: true,
-          src: ['images_src/fixed/*.{gif,jpg,png}'],
-          dest: 'images/',
-          flatten: true,
-        }]
-      },
-    },
-
   });
 
   grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-webp');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images', 'webp']);
 
 };
--- README.md
+++ README.md
@@ -1,21 +1,49 @@
-# Responsive Images: Udacity Project  #
+# Responsive Images: final version #
+This is project that I made during my Udacity's Responsive Images [course](https://www.udacity.com/course/responsive-images--ud882). Initially the blog looked like [this](http://udacity.github.io/responsive-images/downloads/RI-Project-Part-1-Start.zip).
+Course journey involves understading responsive images, using grunt to optimize images and automate workflow, I also helped me to go deep in world of  source set `srcset` attribute and `<picture>` element :)
 
-## Requirements: ##
+##Getting Started
 
-* .
-* .
-* .
+Make sure imagemagick and graphicsmagick are installed on your machine
 
-## How does the Repo Work ##
+First you'll to need make sure you have all `node_modules` and optimized images in <mark>images/</mark> directory.  
+`cd` to project's root directory and run the following command:  
+```
+npm install
+```  
+There is one dependency that is not listed in package.json (webp-bin); remember to add that in with  
+```
+npm install webp-bin
+```  
+or just add this line `npm i -D webp-bin` to your `package.json`  
+It's time to run Grunt:  
+```
+grunt
+```
+  
+Please note that the project is configured to use ImageMagick (as opposed to GraphicsMagick) and requires that to be installed.
 
 
 
-[More on the Udacity Front-End Grading Engine](https://github.com/udacity/frontend-grading-engine)
 
-## Decription ##
+##Notes
+I used [this](https://github.com/somerandomdude/grunt-webp) grunt plugin to convert all images in images/ folder to [webp](https://developers.google.com/speed/webp/).  
+This plugin depends on WebP's cwebp encoder. You'll need to install the [WebP Package](https://developers.google.com/speed/webp/download) or use [webp-bin](https://github.com/yuanyan/node-webp-bin).
 
-* There are markup alternatives to using images. In this part of the project, try replacing, adjusting or otherwise removing images that are just adding bytes to the page.
+--> this can be also done with imagemagick command line tools. Simply used the command in terminal
+>mogrify -format jpg *.png
 
-## Support ##
+You can read more over [here](http://www.imagemagick.org/script/mogrify.php)
 
-.
+<hr>
+
+`<picture>`  general practices:  
+  
+  
+* Only use `<picture>` element when you want to server files with diffrent format (here jpeg and webp) or you are dealing with art direction case.  
+* Only use `media` attribute for resolution switching case
+* Now for resolution-switching case we can simply use `srcset` with `img` with either using <mark>1x, 2x</mark> descriptor or <mark>Width</mark> descriptors. Now again there is a limitaion with 1x, 2x.. descriptors you can only have two images (say 2560px wide and 1280px wide) now It would be complete waste on a 320px wide viewport. This can be handle in two ways:  
+	* We use use `media` attribure in `<picture>` to provide different images for diffrent viewport width. For our responsive blog I have used this technique. Providing large 1x, 2x images for larger viewport, medium 1x, 2x for medium sized viewport and small 1x, 2x smaller viewports. Okay I know it's not going good :( but trust me It'll get better :P . Now way we are doing it here is not recommened because we are forcing browser to choose images instead of letting browser decide what is best. So bottom line - *avoid using `media` attribute for resolution switching case*  
+	* Now other way would be much simpler - Use <mark>Width</mark> descripots. Simply use `srcset` with `img` and provide images with different resolution. This is much easier and here we are letting browser decide what is best for user. In our responsive images blog it can done for resolution-switching case as: `<img src="images/default,jpg srcset="large2x.jpg 1600w, large1x.jpg 800, medium2x.jpg 1024w, medium1x.jpg 640px, small.jpg 320px" alt=""` See I told you naa.. I gets better :B. Now a simple question would "what if I want to provide webp support?" Well it's simply use `<picture>` element and there will be no requirment for `media` attribute. Simply use `<source>` with `type` specified as `image/webp` and `image/jpeg`  
+——
+
